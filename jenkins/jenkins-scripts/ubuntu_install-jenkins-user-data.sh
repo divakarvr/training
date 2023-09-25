e342166d6aea3781f0afd6f683fde23fa610463e
praveen varanasi <praveenvaranasi@praveens-MacBook-Air.local>
2023-09-25 06:24:07 +0530

    Added scripts to install jenkins and configure node

---
+++ jenkins/jenkins-scripts/ubuntu_install-jenkins-user-data.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update 
+apt install openjdk-17-jre -y
+sleep 1
+apt-get install jenkins -y
